dbee9c2b767ad663576117e6084bcba03ee40956
johngerving <gervingjohn@gmail.com>
2023-11-12 15:08:18 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,70 @@
+import * as THREE from "three";
+import { SVGLoader } from "three/addons/loaders/SVGLoader.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import * as BufferGeometryUtils from "three/addons/utils/BufferGeometryUtils.js";
+
+// Initialize canvas and renderer, enable antialiasing
+const canvas = document.querySelector("#c");
+const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+
+// Define camera and its properties
+const fov = 75;
+const aspect = 2;
+const near = 0.1;
+const far = 1000;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+
+// Initial camera position - away from origin, looking at origin
+camera.position.y = 3;
+camera.position.z = 3;
+camera.lookAt(0, 0, 0);
+
+// Define scene
+const scene = new THREE.Scene();
+
+// Define orbit controls - render scene whenever camera moves
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.addEventListener("change", render);
+controls.screenSpacePanning = true;
+
+// Render when the window is resized
+window.addEventListener("resize", render);
+
+// White directional lighting
+const directionalLightColor = 0xffffff;
+const directionalLightIntensity = 3;
+const directionalLight = new THREE.DirectionalLight(
+  directionalLightColor,
+  directionalLightIntensity
+);
+directionalLight.position.set(-1, 2, 4);
+scene.add(directionalLight);
+
+// Soft white ambient light
+const ambientLight = new THREE.AmbientLight(0x404040);
+scene.add(ambientLight);
+
+const cubeMaterial = new THREE.MeshPhongMaterial();
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cube);
+
+function render() {
+  // Check if window has been resized and update camera accordingly
+  if (resizeRendererToDisplaySize(renderer)) {
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
+
+  renderer.render(scene, camera); // Render scene
+}
+
+function resizeRendererToDisplaySize(renderer) {
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const needResize = canvas.width !== width || canvas.height !== height; // Check if canvas dimensions do not match client dimensions
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
