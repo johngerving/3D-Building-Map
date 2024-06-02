4353aedb1554ff91ce5eb9d39da626aa48702b30
johngerving <gervingjohn@gmail.com>
2024-06-01 17:55:05 -0700

    Added buildingProps

--- building-map/src/routes/root.jsx
+++ building-map/src/routes/root.jsx
@@ -1,9 +1,12 @@
 import Scene from "./scene.jsx";
+import { useState } from "react";
+import { buildingPropertiesData } from "../assets/buildingProperties.js";
 
 export default function Root() {
+  const [buildingProps, setBuildingProps] = useState(buildingPropertiesData);
   return (
     <>
-      <Scene />
+      <Scene buildingProps={buildingProps} />
     </>
   );
 }
