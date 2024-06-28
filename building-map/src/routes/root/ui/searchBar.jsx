5a9533d024a6f84c87cd3f99e05a58275b039c19
johngerving <gervingjohn@gmail.com>
2024-06-27 18:28:25 -0700

    Added close button

--- building-map/src/routes/root/ui/searchBar.jsx
+++ building-map/src/routes/root/ui/searchBar.jsx
@@ -1,7 +1,31 @@
-export default function SearchBar() {
+import { useState } from "react";
+import CloseIcon from "../../../assets/close-icon.svg?react";
+
+function CloseButton() {
+  const [hover, setHover] = useState(false);
+
   return (
-    <div className="z-10 absolute top-3 left-3">
-      <input className="rounded-3xl w-80 h-12 shadow-sm py-0 pl-6 pr-14"></input>
+    <button
+      onPointerEnter={() => setHover(true)}
+      onPointerLeave={() => setHover(false)}
+      className="absolute right-4 top-1"
+    >
+      <CloseIcon fill={hover ? "#2563EB" : "black"} />
+    </button>
+  );
+}
+
+export default function SearchBar() {
+  const [text, setText] = useState("");
+
+  return (
+    <div className="z-10 absolute top-3 left-3 w-80 h-12">
+      <input
+        className="rounded-3xl w-80 h-12 shadow-sm py-0 pl-6 pr-14"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      ></input>
+      {text.length > 0 ? <CloseButton /> : null}
     </div>
   );
 }
