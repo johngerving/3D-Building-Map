a389dc214c5b14a466653e8393d5fbfb23fd1baf
johngerving <gervingjohn@gmail.com>
2024-06-29 13:16:10 -0700

    Added clear search functionality

--- building-map/src/routes/root/ui/searchBar.jsx
+++ building-map/src/routes/root/ui/searchBar.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import CloseIcon from "../../../assets/close-icon.svg?react";
 
-function CloseButton() {
+function CloseButton({ handleClear }) {
   const [hover, setHover] = useState(false);
 
   return (
     <button
+      onClick={handleClear}
       onPointerEnter={() => setHover(true)}
       onPointerLeave={() => setHover(false)}
       className="absolute right-4 top-1"
@@ -19,6 +20,10 @@ function CloseButton() {
 export default function SearchBar() {
   const [text, setText] = useState("");
 
+  function handleClear() {
+    setText("");
+  }
+
   return (
     <div className="z-10 absolute top-3 left-3 w-80 h-12">
       <input
@@ -27,7 +32,7 @@ export default function SearchBar() {
         onChange={(e) => setText(e.target.value)}
       ></input>
       {/* Only show clear button if input has value */}
-      {text.length > 0 ? <CloseButton /> : null}
+      {text.length > 0 ? <CloseButton handleClear={handleClear} /> : null}
     </div>
   );
 }
