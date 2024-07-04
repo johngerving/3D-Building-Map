dd98ba05f7b7649cbc5216f714b29725195f8772
johngerving <gervingjohn@gmail.com>
2024-07-04 15:54:02 -0700

    Added put floor and form to edit floors

---
+++ building-map/src/api/put.js
@@ -0,0 +1,49 @@
+import {
+  buildingData,
+  floorData,
+  locationData,
+} from "../assets/buildingProperties.js";
+
+/* Mock PUT API Calls */
+
+// Simulate API response time
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+// Edit floor
+export const putFloor = async (data) => {
+  try {
+    const res = {};
+
+    // Find floor by ID
+    const floorIndex = floorData.findIndex(
+      (floor) => floor.floorID == data.floorID
+    );
+    // Return err if floor doesn't exist
+    if (floorIndex == -1) return { err: "Floor not found" };
+
+    // Make copy of old data
+    const oldData = floorData[floorIndex];
+
+    // Update db with new data
+    floorData[floorIndex] = {
+      floorID: oldData.floorID,
+      buildingID: oldData.buildingID,
+      name: data.name,
+      svg: data.svg,
+      scale: data.scale,
+      verticalGap: data.verticalGap,
+      position: data.position,
+      extrudedSections: data.extrudedSections,
+      floorLayer: data.floorLayer,
+      excludedSections: data.excludedSections,
+      extrudeDepth: data.extrudeDepth,
+    };
+
+    // Return the new data of floor
+    res.data = floorData[floorIndex];
+    await sleep(500);
+    return await res;
+  } catch (err) {
+    throw new Error(err);
+  }
+};
--- building-map/src/routes/buildingMap/ui/editorPanel.jsx
+++ building-map/src/routes/buildingMap/ui/editorPanel.jsx
@@ -1,8 +1,158 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useId } from "react";
 import { useOutletContext } from "react-router-dom";
 import { useSpring, animated } from "@react-spring/web";
+import { useQueryClient, useMutation } from "@tanstack/react-query";
 
 import { useCursor } from "./useCursor.jsx";
+import { putFloor } from "../../../api/put.js";
+
+// const usePutFloor = () => {
+//   const queryClient = useQueryClient();
+
+//   return useMutation({
+//     mutationFn: putFloor,
+//     onMutate:
+//   })
+// }
+
+function SingleFloorInfo({ floor, index }) {
+  const nameInputId = useId();
+  const svgInputId = useId();
+  const scaleInputId = useId();
+  const positionXInputId = useId();
+  const positionYInputId = useId();
+  const verticalGapInputId = useId();
+  const extrudedSectionsInputId = useId();
+  const extrudeDepthInputId = useId();
+  const floorLayerInputId = useId();
+
+  const labelClassName = "text-right w-full";
+  return (
+    // Label and input for each field in floor
+    <div className="p-1">
+      <h1 className="text-xl text-slate-700 mb-2">{index + 1}</h1>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "max-content auto",
+          gridGap: "10px",
+          paddingLeft: "1rem",
+        }}
+      >
+        <label className={labelClassName} htmlFor={nameInputId}>
+          Name
+        </label>
+        <input
+          type="text"
+          id={nameInputId}
+          name="name"
+          defaultValue={floor.name}
+          className="border"
+        />
+
+        <label className={labelClassName} htmlFor={svgInputId}>
+          SVG
+        </label>
+        <input
+          type="text"
+          id={svgInputId}
+          name="svg"
+          defaultValue={floor.svg}
+          className="border"
+        />
+
+        <label className={labelClassName} htmlFor={scaleInputId}>
+          Scale
+        </label>
+        <input
+          type="text"
+          id={scaleInputId}
+          name="scale"
+          defaultValue={floor.scale}
+          className="border"
+        />
+
+        <label className={labelClassName} htmlFor={positionXInputId}>
+          Position X
+        </label>
+        <input
+          type="text"
+          id={positionXInputId}
+          name="positionX"
+          defaultValue={floor.position[0]}
+          className="border"
+        />
+
+        <label className={labelClassName} htmlFor={positionYInputId}>
+          Position Y
+        </label>
+        <input
+          type="text"
+          id={positionYInputId}
+          name="positionY"
+          defaultValue={floor.position[1]}
+          className="border"
+        />
+
+        <label className={labelClassName} htmlFor={verticalGapInputId}>
+          Vertical Gap
+        </label>
+        <input
+          type="text"
+          id={verticalGapInputId}
+          name="verticalGap"
+          defaultValue={floor.verticalGap}
+          className="border"
+        />
+
+        <label className={labelClassName} htmlFor={extrudedSectionsInputId}>
+          Extruded Sections
+        </label>
+        <input
+          type="text"
+          id={extrudedSectionsInputId}
+          name="extrudedSections"
+          defaultValue={floor.extrudedSections}
+          className="border"
+        />
+
+        <label className={labelClassName} htmlFor={extrudeDepthInputId}>
+          Extrude Depth
+        </label>
+        <input
+          type="text"
+          id={extrudeDepthInputId}
+          name="extrudeDepth"
+          defaultValue={floor.extrudeDepth}
+          className="border"
+        />
+
+        <label className={labelClassName} htmlFor={floorLayerInputId}>
+          Floor Layer
+        </label>
+        <input
+          type="text"
+          id={floorLayerInputId}
+          name="floorLayer"
+          defaultValue={floor.floorLayer}
+          className="border"
+        />
+      </div>
+    </div>
+  );
+}
+
+function FloorInfo({ floors }) {
+  const labelClassName = "text-right";
+
+  return (
+    <div className="p-4 pr-3">
+      {floors.map((floor, index) => {
+        return <SingleFloorInfo key={index} floor={floor} index={index} />;
+      })}
+    </div>
+  );
+}
 
 export function EditorPanel() {
   const [
@@ -26,15 +176,14 @@ export function EditorPanel() {
   useEffect(() => {
     // On mouse down, if the resizer was clicked, enable resizing
     function handleMouseDown(e) {
-      e.preventDefault();
       if (e.target.classList.contains("resize")) {
+        e.preventDefault();
         setIsResizing(true);
       }
     }
 
     // On mouse up, disable resizing no matter what
     function handleMouseUp(e) {
-      e.preventDefault();
       setIsResizing(false);
     }
 
@@ -63,13 +212,16 @@ export function EditorPanel() {
       style={{ width: width }}
       className={`z-10 absolute top-0 left-0 h-full bg-white`}
     >
+      <div id="scroll-container" className="h-full mr-2 overflow-y-scroll">
+        <FloorInfo floors={floors} />
+      </div>
       <div
         id="resize"
         onMouseEnter={() => setIsHovering(true)}
         onMouseLeave={() => setIsHovering(false)}
         // Resizer should change cursor on hover if resizing is not enabled
         // Resizer should be blue if hovered over or resizing is enabled
-        className={`resize z-10 float-right w-2 h-full bg-white shadow-md ${
+        className={`resize z-10 absolute top-0 right-0 w-2 h-full bg-white shadow-md ${
           !isResizing ? "hover:cursor-col-resize" : ""
         } ${isHovering || isResizing ? "bg-blue-500" : ""}`}
       ></div>
