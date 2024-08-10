0ea390a6f38acafa37517bcb8fb86d9bbd6f194a
johngerving <gervingjohn@gmail.com>
2024-08-09 23:08:28 -0700

    Removed https from backend URL

--- building-map/src/http-common.js
+++ building-map/src/http-common.js
@@ -1,2 +1,2 @@
-export const baseURL = "https://3dbuildingmap-backend.azurewebsites.net:80";
+export const baseURL = "http://3dbuildingmap-backend.azurewebsites.net:80";
 // export const baseURL = "http://localhost:8080";
