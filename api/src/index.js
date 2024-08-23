8491bd20d824b3ac60556e575d67116b0614c752
johngerving <gervingjohn@gmail.com>
2024-08-23 14:41:02 -0700

    Changed sameSite to None

--- api/src/index.js
+++ api/src/index.js
@@ -31,7 +31,7 @@ app.use(
     secret: [process.env.COOKIE_SECRET],
     cookie: {
       secure: process.env.NODE_ENV === "production" ? true : "auto",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
       maxAge: 1000 * 60 * 60 * 24,
       domain:
         process.env.NODE_ENV === "production" ? process.env.COOKIE_DOMAIN : "",
